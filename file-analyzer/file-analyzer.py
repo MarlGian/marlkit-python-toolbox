8359a3748be7f7e5be351ccdab98ce17a5a54008
peeb. <106175736+MarlGian@users.noreply.github.com>
2026-03-02 22:55:13 +0800

    Refactor file-analyzer.py for consistency and error handling
    Refactor return statements for consistency and readability. Added error handling for file not found.

--- file-analyzer/file-analyzer.py
+++ file-analyzer/file-analyzer.py
@@ -1,26 +1,28 @@
 import sys
 
+
 def word_count(files):
     with open(files, "r") as file:
         word_count = file.read().replace(",", " ").split()
 
-    return(f"{len(word_count)} total words in {files}")
-    
-    
+    return (f"{len(word_count)} total words in {files}")
+
+
 def letter_count(file):
     with open(file, "r") as f:
         letter_counts = f.read()
         letters = [char for char in letter_counts if char.isalpha()]
-        
-    return(f"{len(letters)} total letters in {file}")
-    
-  
+
+    return (f"{len(letters)} total letters in {file}")
+
+
 def number_count(file):
     with open(file, "r") as f:
         num_read = f.read()
         num = [char for char in num_read if char.isdigit()]
-        
-    return(f"{len(num)} total numbers in {file}")
+
+    return (f"{len(num)} total numbers in {file}")
+
 
 def word_frequency(file):
     unique = {}
@@ -37,26 +39,29 @@ def word_frequency(file):
 
     result = ""
     for key, value in sorted(unique.items()):
-        result += f"{key} : {value}\n"    
+        result += f"{key} : {value}\n"
     return result
 
-txtfile = sys.argv[1]
-    
 
-if len(sys.argv) < 3:
+
+
+if len(sys.argv) < 3 or sys.argv[0] == "-help":
     print("Usage: python file-analyzer.py <filename> <flag>")
     print("Flags: --count-let, --count-word, --count-num, --word-freq")
     sys.exit()
 
 arg3 = sys.argv[2]
-
-if arg3 == "--count-let:":
-    print(letter_count(txtfile))
-elif arg3 == "--count-word":
-    print(word_count(txtfile))
-elif arg3 == "--count-num":
-    print(number_count(txtfile))
-elif arg3 == "--word-freq":
-    print(word_frequency(txtfile))
-else:
-     print("Flags: --count-let, --count-word, --count-num, --word-freq")
+txtfile = sys.argv[1]
+try:
+    if arg3 == "--count-let":
+        print(letter_count(txtfile))
+    elif arg3 == "--count-word":
+        print(word_count(txtfile))
+    elif arg3 == "--count-num":
+        print(number_count(txtfile))
+    elif arg3 == "--word-freq":
+        print(word_frequency(txtfile))
+    else:
+        print("Flags: --count-let, --count-word, --count-num, --word-freq")
+except FileNotFoundError:
+    print("File not found! Are you sure the file exists?")
